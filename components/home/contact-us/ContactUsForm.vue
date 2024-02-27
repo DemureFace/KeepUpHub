2807a887b5dcd3d45e6da36f5219e1f85465dff3
Mikhail Baydakov <m.baedakov@gmail.com>
2024-02-27 23:43:40 +0200

    fix

--- components/home/contact-us/ContactUsForm.vue
+++ components/home/contact-us/ContactUsForm.vue
@@ -99,7 +99,6 @@ function setFieldPosition(name) {
       type="submit"
       widthClass="w-full md:w-fit"
       :is-loading="isFormSending"
-      class="z-[-1]"
     >
       {{ $t("contac_us_section.form.button_text") }}
     </BaseButton>
